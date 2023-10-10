ac9887a81ccbf2194fc650c0b51a20513c15fce5
Abhinav <abhinavdogra649@gmail.com>
2023-10-09 18:44:44 -0700

    mdoels update

--- portal/models.py
+++ portal/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 # Create your models here.
-
 class contact_details(models.Model):
     Full_name=models.CharField(max_length=200, null=False,blank=False)
     email=models.EmailField(max_length=200, null=False, blank=False)
